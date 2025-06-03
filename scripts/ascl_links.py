bf576c9f03f9890f88977ac42c392135ecf78034
Pranath Reddy Kumbam <pranath.mail@gmail.com>
2025-06-03 13:57:08 -0500

    ASCL Pipeline update
    Added downstream classifier to the ASCL script

--- scripts/ascl_links.py
+++ scripts/ascl_links.py
@@ -1,10 +1,13 @@
 import sys
 import pandas as pd
 from gcd.ascl_search import fetch_and_process_ascl_links
+from gcd.classification_utils import run_classification_pipeline
 from config import GITHUB_TOKEN
 
 def main():
     fetch_and_process_ascl_links(token=GITHUB_TOKEN)
+    run_classification_pipeline(input_csv_path="./ascl_new_github_repo_readme.csv", 
+                                output_csv_path="./ascl_new_github_repo_readme_classified.csv")
 
 if __name__ == "__main__":
     main()
