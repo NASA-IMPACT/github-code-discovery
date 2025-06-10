abdda16def9ee873d5f90c801cc76b89324629bd
Pranath Reddy Kumbam <pranath.mail@gmail.com>
2025-06-10 14:33:12 -0500

    Update doi_pipeline.py

--- gcd/doi_pipeline.py
+++ gcd/doi_pipeline.py
@@ -4,6 +4,7 @@ from gcd.pdf_scraper import process_dois_parallel, download_all_pdfs_from_csvs
 from gcd.extract_links import extract_links_from_pdf_folder
 from gcd.github_api_ql import process_github_links_from_csv
 from gcd.classification_utils import run_classification_pipeline
+import os
 
 def doi_pipeline(token: str, csv_path: str, start_row: int = 0, end_row: int = 1000):
     process_dois_parallel(csv_path, start_row, end_row, n_jobs=-1)
@@ -15,6 +16,11 @@ def doi_pipeline(token: str, csv_path: str, start_row: int = 0, end_row: int = 1
                                   csv_output_path="./doi_results/github_repo_readme.csv")
     run_classification_pipeline(input_csv_path="./doi_results/github_repo_readme.csv", 
                                 output_csv_path="./doi_results/github_repo_readme_classified.csv")
+    
+    # Uncomment to save all the results
+    os.remove("./doi_results/extracted_github_links.csv")
+    os.remove("./doi_results/github_repo_readme.json")
+    os.remove("./doi_results/github_repo_readme.csv")
 
 def run_doi_pipeline(token: str, csv_path: str, start_row: int = 0, end_row: int = 1000):
     doi_pipeline(token, csv_path, start_row, end_row)
